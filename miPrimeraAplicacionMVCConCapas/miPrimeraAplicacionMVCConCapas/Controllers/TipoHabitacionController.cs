2c08f0464748d9c36566c63588e0b2a0414b52e4
Gustavo Sicaja <gustavo.sicaja@ssasa.com>
2023-08-21 14:38:17 -0600

    Actualizacion de librerias, agregado sweetalert2

---
+++ miPrimeraAplicacionMVCConCapas/miPrimeraAplicacionMVCConCapas/Controllers/TipoHabitacionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace miPrimeraAplicacionMVCConCapas.Controllers
+{
+    public class TipoHabitacionController : Controller
+    {
+        // GET: TipoHabitacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
